c2f1c7e1c8205195b46ee65f9bad25fb546fbf55
DLehenbauer <DLehenbauer@users.noreply.github.com>
2018-04-28 11:44:54 -0700

    WIP: Move '_noteToPitch' to .h

--- src/emcc-firmware.bat
+++ src/emcc-firmware.bat
@@ -1,4 +1,4 @@
 setlocal
 set SrcPath=%CD%\firmware\arduino-gm-synth
-emcc --bind -o %CD%\firmware\firmware.js -O0 -g -std=c++11 -DF_CPU=16000000 -I%SrcPath%\emscripten %SrcPath%\emscripten\avr\mocks.cpp %SrcPath%\emscripten\bindings.cpp %SrcPath%\main.cpp %SrcPath%\synth.cpp %SrcPath%\instruments.cpp %SrcPath%\midi.cpp
+emcc --bind -o %CD%\firmware\firmware.js -O0 -g -std=c++11 -DF_CPU=16000000 -I%SrcPath%\emscripten %SrcPath%\emscripten\avr\mocks.cpp %SrcPath%\emscripten\bindings.cpp %SrcPath%\main.cpp %SrcPath%\synth.cpp %SrcPath%\instruments.cpp
 endlocal
--- src/firmware/arduino-gm-synth/synth.h
+++ src/firmware/arduino-gm-synth/synth.h
@@ -7,6 +7,7 @@
 #include "instruments.h"
 #include "lerp.h"
 #include "drivers/dac/ltc16xx.h"
+#include <math.h>
 
 #define DAC
 
@@ -14,6 +15,9 @@
 	#define DAC
 	void TIMER2_COMPA_vect();
 #endif
+constexpr static uint16_t pitch(double sampleRate, double note) {
+	return round(pow(2, (note - 69.0) / 12.0) * 440.0 / sampleRate * static_cast<double>(0xFFFF));
+}
 
 class Synth {
     public:
@@ -23,6 +27,40 @@ class Synth {
 		constexpr static double sampleRate = static_cast<double>(F_CPU) / 8.0 / static_cast<double>(Synth::samplingInterval);
 		
 	private:
+		static constexpr uint8_t offsetTable[] = { 0, 0, 1, 1, 2, 3, 3, 3 };
+
+		// Map MIDI notes [0..127] to the corresponding Q8.8 sampling interval
+		constexpr static uint16_t _noteToPitch[] PROGMEM = {
+#ifndef __EMSCRIPTEN__
+			pitch(sampleRate, 0x00), pitch(sampleRate, 0x01), pitch(sampleRate, 0x02), pitch(sampleRate, 0x03), pitch(sampleRate, 0x04), pitch(sampleRate, 0x05), pitch(sampleRate, 0x06), pitch(sampleRate, 0x07),
+			pitch(sampleRate, 0x08), pitch(sampleRate, 0x09), pitch(sampleRate, 0x0A), pitch(sampleRate, 0x0B), pitch(sampleRate, 0x0C), pitch(sampleRate, 0x0D), pitch(sampleRate, 0x0E), pitch(sampleRate, 0x0F),
+			pitch(sampleRate, 0x10), pitch(sampleRate, 0x11), pitch(sampleRate, 0x12), pitch(sampleRate, 0x13), pitch(sampleRate, 0x14), pitch(sampleRate, 0x15), pitch(sampleRate, 0x16), pitch(sampleRate, 0x17),
+			pitch(sampleRate, 0x18), pitch(sampleRate, 0x19), pitch(sampleRate, 0x1A), pitch(sampleRate, 0x1B), pitch(sampleRate, 0x1C), pitch(sampleRate, 0x1D), pitch(sampleRate, 0x1E), pitch(sampleRate, 0x1F),
+			pitch(sampleRate, 0x20), pitch(sampleRate, 0x21), pitch(sampleRate, 0x22), pitch(sampleRate, 0x23), pitch(sampleRate, 0x24), pitch(sampleRate, 0x25), pitch(sampleRate, 0x26), pitch(sampleRate, 0x27),
+			pitch(sampleRate, 0x28), pitch(sampleRate, 0x29), pitch(sampleRate, 0x2A), pitch(sampleRate, 0x2B), pitch(sampleRate, 0x2C), pitch(sampleRate, 0x2D), pitch(sampleRate, 0x2E), pitch(sampleRate, 0x2F),
+			pitch(sampleRate, 0x30), pitch(sampleRate, 0x31), pitch(sampleRate, 0x32), pitch(sampleRate, 0x33), pitch(sampleRate, 0x34), pitch(sampleRate, 0x35), pitch(sampleRate, 0x36), pitch(sampleRate, 0x37),
+			pitch(sampleRate, 0x38), pitch(sampleRate, 0x39), pitch(sampleRate, 0x3A), pitch(sampleRate, 0x3B), pitch(sampleRate, 0x3C), pitch(sampleRate, 0x3D), pitch(sampleRate, 0x3E), pitch(sampleRate, 0x3F),
+			pitch(sampleRate, 0x40), pitch(sampleRate, 0x41), pitch(sampleRate, 0x42), pitch(sampleRate, 0x43), pitch(sampleRate, 0x44), pitch(sampleRate, 0x45), pitch(sampleRate, 0x46), pitch(sampleRate, 0x47),
+			pitch(sampleRate, 0x48), pitch(sampleRate, 0x49), pitch(sampleRate, 0x4A), pitch(sampleRate, 0x4B), pitch(sampleRate, 0x4C), pitch(sampleRate, 0x4D), pitch(sampleRate, 0x4E), pitch(sampleRate, 0x4F),
+			pitch(sampleRate, 0x50), pitch(sampleRate, 0x51), pitch(sampleRate, 0x52), pitch(sampleRate, 0x53), pitch(sampleRate, 0x54), pitch(sampleRate, 0x55), pitch(sampleRate, 0x56), pitch(sampleRate, 0x57),
+			pitch(sampleRate, 0x58), pitch(sampleRate, 0x59), pitch(sampleRate, 0x5A), pitch(sampleRate, 0x5B), pitch(sampleRate, 0x5C), pitch(sampleRate, 0x5D), pitch(sampleRate, 0x5E), pitch(sampleRate, 0x5F),
+			pitch(sampleRate, 0x60), pitch(sampleRate, 0x61), pitch(sampleRate, 0x62), pitch(sampleRate, 0x63), pitch(sampleRate, 0x64), pitch(sampleRate, 0x65), pitch(sampleRate, 0x66), pitch(sampleRate, 0x67),
+			pitch(sampleRate, 0x68), pitch(sampleRate, 0x69), pitch(sampleRate, 0x6A), pitch(sampleRate, 0x6B), pitch(sampleRate, 0x6C), pitch(sampleRate, 0x6D), pitch(sampleRate, 0x6E), pitch(sampleRate, 0x6F),
+			pitch(sampleRate, 0x70), pitch(sampleRate, 0x71), pitch(sampleRate, 0x72), pitch(sampleRate, 0x73), pitch(sampleRate, 0x74), pitch(sampleRate, 0x75), pitch(sampleRate, 0x76), pitch(sampleRate, 0x77),
+			pitch(sampleRate, 0x78), pitch(sampleRate, 0x79), pitch(sampleRate, 0x7A), pitch(sampleRate, 0x7B), pitch(sampleRate, 0x7C), pitch(sampleRate, 0x7D), pitch(sampleRate, 0x7E), pitch(sampleRate, 0x7F),
+#else
+			// Map MIDI notes [0..127] to the corresponding Q8.8 sampling interval at ~20 kHz
+			0x001B, 0x001D, 0x001E, 0x0020, 0x0022, 0x0024, 0x0026, 0x0029, 0x002B, 0x002E, 0x0030, 0x0033, 0x0036, 0x0039, 0x003D, 0x0040,
+			0x0044, 0x0048, 0x004D, 0x0051, 0x0056, 0x005B, 0x0060, 0x0066, 0x006C, 0x0073, 0x0079, 0x0081, 0x0088, 0x0090, 0x0099, 0x00A2,
+			0x00AC, 0x00B6, 0x00C1, 0x00CC, 0x00D8, 0x00E5, 0x00F3, 0x0101, 0x0111, 0x0121, 0x0132, 0x0144, 0x0158, 0x016C, 0x0182, 0x0199,
+			0x01B1, 0x01CB, 0x01E6, 0x0203, 0x0221, 0x0242, 0x0264, 0x0289, 0x02AF, 0x02D8, 0x0303, 0x0331, 0x0362, 0x0395, 0x03CC, 0x0406,
+			0x0443, 0x0484, 0x04C9, 0x0511, 0x055E, 0x05B0, 0x0607, 0x0663, 0x06C4, 0x072B, 0x0798, 0x080B, 0x0886, 0x0908, 0x0991, 0x0A23,
+			0x0ABD, 0x0B60, 0x0C0E, 0x0CC5, 0x0D87, 0x0E55, 0x0F30, 0x1017, 0x110C, 0x120F, 0x1322, 0x1445, 0x157A, 0x16C1, 0x181B, 0x198A,
+			0x1B0F, 0x1CAB, 0x1E5F, 0x202D, 0x2217, 0x241E, 0x2644, 0x288B, 0x2AF4, 0x2D82, 0x3036, 0x3314, 0x361E, 0x3955, 0x3CBE, 0x405B,
+			0x442F, 0x483C, 0x4C88, 0x5115, 0x55E7, 0x5B03, 0x606C, 0x6628, 0x6C3B, 0x72AB, 0x797C, 0x80B6, 0x885D, 0x9079, 0x9910, 0xA22A,
+#endif
+		};
+
 		static volatile const int8_t*	v_wave[Synth::numVoices];			// Starting address of 256b wave table.
 		static volatile uint16_t		v_phase[Synth::numVoices];			// Phase accumulator holding the Q8.8 offset of the next sample.
 		static volatile uint16_t		v_pitch[Synth::numVoices];			// Q8.8 sampling period, used to advance the '_phase' accumulator.
@@ -50,6 +88,8 @@ class Synth {
 	public:
         void begin();
         
+		// Returns the next idle voice, if any.  If no voice is idle, uses ADSR stage and amplitude to
+		// choose the best candidate for note-stealing.
 		uint8_t getNextVoice() {
 			uint8_t current = maxVoice;
 			uint8_t currentStage;
@@ -89,6 +129,7 @@ class Synth {
 			return current;
 		}
         
+
 		void noteOn(uint8_t voice, uint8_t note, uint8_t velocity, const Instrument& instrument);
         
 		void noteOff(uint8_t voice);
